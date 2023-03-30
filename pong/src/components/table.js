a1b70a0d1dc9a2f5dc423c7f0982137699d9182e
Edwar-Sanz <ssanzjhon5@gmail.com>
2023-03-30 00:26:16 -0500

    se agregan puntajes

--- pong/src/components/table.js
+++ pong/src/components/table.js
@@ -14,7 +14,7 @@ function Table() {
     const [player2Left, setPlayer2Left] = useState(0);
     const [move, setmove] = useState(1);
     
-    
+   
     // cuando renderise, verifica si ya se puede acceder a elemento
     //elemento === truthy
     useEffect(() => {
@@ -41,7 +41,7 @@ function Table() {
                 setPlayer2top(player2top - 20);
                 if (player2top <= 20) {setmove(0)};
             }
-        }, 700);
+        }, 40);
 
         return () => {
         clearTimeout(timerId);
@@ -74,15 +74,15 @@ function Table() {
     function reducerBall(ballState, dispatch) {
         switch (dispatch.estado) {
             case 0:  
-                return {...ballState, "top": ballState.top + 20,"left": ballState.left + 20,"estado": 0}
+                return {...ballState, "top": ballState.top + 23,"left": ballState.left + 23,"estado": 0}
             case 1:  
-                return {...ballState, "top": ballState.top - 20,"left": ballState.left + 20,"estado": 1}
+                return {...ballState, "top": ballState.top - 23,"left": ballState.left + 23,"estado": 1}
             case 2:  
-                return {...ballState, "top": ballState.top + 20,"left": ballState.left - 20,"estado": 2}
+                return {...ballState, "top": ballState.top + 23,"left": ballState.left - 23,"estado": 2}
             case 3:  
-                return {...ballState, "top": ballState.top - 20,"left": ballState.left - 20,"estado": 3}
+                return {...ballState, "top": ballState.top - 23,"left": ballState.left - 23,"estado": 3}
             default:
-                return {...ballState, "top": ballState.top + 20,"left": ballState.left + 20,"estado": 0}
+                return {...ballState, "top": ballState.top + 23,"left": ballState.left + 23,"estado": 0}
         }
         
     }
@@ -97,33 +97,46 @@ function Table() {
         let top = ballState.top;
         let left = ballState.left;
         if      (top+25  >= refH && est === 0) {dispatch({"estado": 1})}
-        else if (left+25 >= refW && est === 0) {dispatch({"estado": 2})}
-        else if (left+25 >= refW && est === 1) {dispatch({"estado": 3})}
         else if (top+25  >= refH && est === 2) {dispatch({"estado": 3})}
         else if (top     <= 5    && est === 3) {dispatch({"estado": 2})}
         else if (top     <= 5    && est === 1) {dispatch({"estado": 0})}
-        else if (left    <= 5    && est === 3) {dispatch({"estado": 1})}
-        else if (left    <= 5    && est === 2) {dispatch({"estado": 0})}
+        else if (left+25 >= refW && est === 0 && (top + 25 < player2top || top > player2top + 100)) {setP1score(p1score + 1); dispatch({"estado": 2})} //point to p1
+        else if (left+25 >= refW && est === 1 && (top + 25 < player2top || top > player2top + 100)) {setP1score(p1score + 1); dispatch({"estado": 3})} //point to p1 
+        else if (left+65 >= refW && est === 0 && ((top > player2top && top < player2top + 100) || (top+25 > player2top && top+25 < player2top + 100))) {dispatch({"estado": 2})} 
+        else if (left+65 >= refW && est === 1 && ((top > player2top && top < player2top + 100) || (top+25 > player2top && top+25 < player2top + 100))) {dispatch({"estado": 3})}  
+        
+        else if (left    <= 5    && est === 3 && (top + 25 < player1top || top > player1top + 100)) {setP2score(p2score + 1); dispatch({"estado": 1})} //point to p2
+        else if (left    <= 5    && est === 2 && (top + 25 < player1top || top > player1top + 100)) {setP2score(p2score + 1); dispatch({"estado": 0})} //point to p2
+        else if (left    <= 25    && est === 3 && ((top > player1top && top < player1top + 100) || (top+25 > player1top && top+25 < player1top + 100))) {dispatch({"estado": 1})}
+        else if (left    <= 25    && est === 2 && ((top > player1top && top < player1top + 100) || (top+25 > player1top && top+25 < player1top + 100))) {dispatch({"estado": 0})} 
         else{dispatch({"estado": est})}
     }
 
     useEffect(() => {
-        const timerBall = setTimeout(()=>mover(ballState), 500);
+        const timerBall = setTimeout(()=>mover(ballState), 40);
         return () => {clearTimeout(timerBall);};
-    }, [ballState]);
+    });
 
-//-------------------------------------------------    
+//-----------------puntajes--------------------------------
+    const [p1score, setP1score] = useState(0);
+    const [p2score, setP2score] = useState(0);
+
 
 
 //---------------------------------------------------
     return (
         <>
+            
             <div className="table" id="table" ref={tableRef}>
+                <div className="line"></div>
                 <Ball top={ballState.top} left={ballState.left} />
                 <Player top={player1top} left={5} />
                 <Player top={player2top} left={player2Left} />
             </div>
-            <button onClick={()=>console.log(player1top, player1top+100)}> clik me </button>
+
+            <div className="puntaje1"> PUNTAJE JUGADOR 1: {p1score}</div>
+            <div className="puntaje2"> PUNTAJE JUGADOR 2: {p2score}</div>
+            
         </>
     );
 }
