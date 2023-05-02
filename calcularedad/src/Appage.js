8f58d2fcb4533ca262a177e32443a13d33590a18
Edwar-Sanz <ssanzjhon5@gmail.com>
2023-05-01 19:30:24 -0500

    Se agregan cálculos

--- calcularedad/src/Appage.js
+++ calcularedad/src/Appage.js
@@ -1,4 +1,4 @@
-import './App.css';
+import './Appage.css';
 import * as React from 'react';
 import Box from '@mui/material/Box';
 import InputLabel from '@mui/material/InputLabel';
@@ -11,42 +11,58 @@ import { useState } from 'react';
 
 
 
-
+let fechaActual = new Date();
 function BasicSelect({getBorn}) {
+  // ----------- generar lista de dias mes-------------
+  function range(start, stop) {
+    let arr = [];
+    for (let i = start; i < stop; i ++) {
+      arr.push(i);
+    }
+    return arr;
+  }
+  const days = range(1, 32);
+  // ----------- estados-------------
   const [month, setMonth] = useState('');
   const [year, setYear] = useState('');
   const [day, setDay] = useState('');
-  const [validations, setValidations] = useState(true);
-
+  const [monthError, setMonthError] = useState(false);
+  const [yearError, setYearError] = useState(false);
+  const [dayError, setDayError] = useState(false);
   
-
+  // ----------- manejadores-------------
   const handleChangeMonth = (event) => {setMonth(event.target.value); };
-  const handleChangeyear = (event) => { setYear(event.target.value);};
   const handleChangeDay = (event) => { setDay(event.target.value);};
-
-
-    // console.log(event.target.value)
-    // if (event.target.value !== 0 && 
-    //   event.target.value !== NaN &&
-    //   event.target.value > 0){ 
-      //   }else{
-    //     setYear("hola"); 
-    //   }
-    
-    function range(start, stop) {
-      let arr = [];
-      for (let i = start; i < stop; i ++) {
-        arr.push(i);
+  const handleChangeyear = (event) => {
+    if (Number(event.target.value) !== 0 && 
+      !isNaN(Number(event.target.value)) &&
+      Number(event.target.value) > 0 &&
+      Number(event.target.value) <= fechaActual.getFullYear() ){ 
+        setYear(Number(event.target.value));
+      }else{
+      setYear("");
       }
-      return arr;
+  };
+
+  const handleCalculations = () =>{
+    if (month !== "" && day !== "" && year !== "" && year > 99) {
+      getBorn(month, day, year) 
+      setMonthError(false) 
+      setDayError(false) 
+      setYearError(false);
+    }else if (month === "") {
+      setMonthError(true)
+    }else if (day === "") {
+      setDayError(true)
+    }else if (year === "") {
+      setYearError(true)
     }
-    const days = range(1, 32)
-  
+  }
 
   return (
     <Box >
         {/* //---------------month------------------------ */}
-        <FormControl sx={{ minWidth: 120 }} >
+        <FormControl error={monthError} sx={{ width: 190, marginLeft:"5px", marginTop:"5px" }} >
         <InputLabel >Month</InputLabel>
         <Select 
           value={month}
@@ -66,9 +82,9 @@ function BasicSelect({getBorn}) {
           <MenuItem value={11}>Noviembre</MenuItem>
           <MenuItem value={12}>Diciembre</MenuItem>
         </Select>
-      {/* //---------------day------------------------ */}
       </FormControl>
-        <FormControl sx={{ minWidth: 120 }} >
+      {/* //---------------day------------------------ */}
+      <FormControl error={dayError} sx={{ width: 190, marginLeft:"5px", marginTop:"5px" }} >
         <InputLabel >Day</InputLabel>
         <Select 
           value={day}
@@ -84,8 +100,8 @@ function BasicSelect({getBorn}) {
         </Select>   
       </FormControl>
       {/* //---------------year------------------------ */}
-      <TextField label="Year" variant="outlined" onChange={handleChangeyear}/>
-      <Button onClick={()=> getBorn()}>send</Button>
+      <TextField error={yearError} sx={{ width: 190, marginLeft:"5px", marginTop:"5px" }} value={year} label="Year" variant="outlined" onChange={handleChangeyear}/>
+      <Button variant="contained" sx={{ width: 190, marginLeft:"5px", marginTop:"15px", height:"100%" }} onClick={()=> handleCalculations()}> CALCULAR</Button>
     </Box>
   );
 }
@@ -93,13 +109,22 @@ function BasicSelect({getBorn}) {
 //console.log(fechaActual - year, day, month)
 function Appage() {
   const [date, setDate] = useState('');
-  let fechaActual = new Date();
-  const getBorn = (validations, year, month, day) => { setDate()};
+  
+  const getBorn = ( month, day, year) => { 
+    let born = new Date(`${month}/${day}/${year}`)
+    
+    let milliseconds = fechaActual-born;
+    let years = Math.trunc(milliseconds / (1000*60*60*24*30.4166666666666*12));
+    let months = Math.trunc(milliseconds % (1000*60*60*24*30.4166666666666*12) / (1000*60*60*24*30.4166666666666));
+    let dias = Math.trunc(((milliseconds % (1000*60*60*24*30.4166666666666*12)) % (1000*60*60*24*30.4166666666666)) / (1000*60*60*24));
+    //console.log(years, months, dias)
+    setDate(`, ${years} años, ${months} meses, ${dias} días`)
+  };
 
   return (
-    <div className="App">
+    <div className="App" style={{marginTop:"100px", padding:"15px", maxWidth:"700px"}}>
       <BasicSelect getBorn={getBorn} />
-      <div>Tu edad es de: {date}</div>
+      <div style={{marginTop:"50px", fontSize:"22px"}} >Su edad es de{date}</div>
     </div>
   );
 }
--- calcularedad/src/index.js
+++ calcularedad/src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import Appage from './Appage';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
